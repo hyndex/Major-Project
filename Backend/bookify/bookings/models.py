ece8ece095efc841e34fbb68c1d7d97bd908cc7b
Diki <dikibhuyan@gmail.com>
2020-11-02 21:31:21 +0530

    backend started

---
+++ Backend/bookify/bookings/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+import datetime as dt
+
+
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=150, blank=False, null=True, default = '')
+    phone = models.CharField(max_length=20, blank=False, null=True, default = '')
+    address = models.CharField(max_length=150, blank=True, null=True)
+    status = models.CharField(max_length=10, blank=True, null=True)
+    image = models.ImageField(upload_to='',blank=True, null=True)
+    date_updated = models.DateTimeField(default=dt.datetime.now(), blank=True)
+    
+    def __str__(self):
+        return self.user.username
+
+class Charger(models.Model):
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)  
+    address = models.CharField(max_length=150, blank=True, null=True)
+    status = models.CharField(max_length=10, blank=True, null=True)
+    date_updated = models.DateTimeField(default=dt.datetime.now(), blank=True)
+    
+    def __str__(self):
+        return self.user.username
+
+
+class Bookings(models.Model):
+    user=models.ForeignKey(Profile, on_delete=models.CASCADE)
+    charger=models.ForeignKey(Charger, on_delete=models.CASCADE)
+    status = models.CharField(max_length=10, blank=True, null=True)
+    date_updated = models.DateTimeField(default=dt.datetime.now(), blank=True)  
+    def __str__(self):
+        return str(self.user.user.username+' '+self.charger.id+' '+self.date_updated)
+
---
+++ Backend/bookify/bookings/serializers.py
@@ -0,0 +1,95 @@
+from rest_framework import serializers
+from rest_framework import exceptions
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+# from .models import *
+# from .permissions import *
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields=('username','password','email')
+        write_only_fields=('password',)
+
+class ProfileSerializer(serializers.ModelSerializer):
+    user = UserSerializer(required=True)
+    class Meta:
+        model = Profile
+        fields = ('id','user','name','phone','address','status','image')
+        read_only_fields=('image',)
+
+    def create(self, validated_data):
+        user_data = validated_data.pop('user')
+        user = User.objects.create(username=str(user_data['username']).lower(),
+                            email=str(user_data['email']).lower(),
+                            )
+        user.set_password(user_data['password'])
+        user.save()
+        try:
+            profile = Profile.objects.create(user=user,
+                                phone=validated_data.pop('phone'),
+                                address=validated_data.pop('address'),
+                                status=validated_data.pop('status'),
+                                corp= Institute.objects.get(user=self.context['request'].user)
+                                )
+            return profile
+        except:
+            User.objects.filter(username=user_data['username']).delete()
+        return {"False"}
+
+
+class ChargerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Charger
+        fields=('id','address','status','date_updated','user')
+        read_only_fields=('id','date_updated','user')
+
+    def create(self, validated_data):
+        user= Profile.objects.get(user=self.context['request'].user)
+        charger=Charger.objects.create(user=user,**validated_data)
+        return charger
+
+
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Bookings
+        fields=('id','user','status','charger','date_updated')
+        read_only_fields=('date_updated','user','id',)
+    
+    def create(self, validated_data):
+        user= Profile.objects.get(user=self.context['request'].user)
+        charger=Charger.objects.create(user=user,**validated_data)
+        return charger
+
+
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, data):
+        username = data.get('username','')
+        password = data.get('password','')
+
+        if username and password:#checking if both are avalable or not
+            user = authenticate(username=username,password=password)
+            if user:# if user found 
+                if user.is_active:# if user is active
+                    data['user'] = user #if all correct then we are going to add "user" to given "data" and return 
+                else:#if account is not active reise or active the account
+                    msg = 'account is not active'
+                    raise exceptions.ValidationError(msg)
+            else:
+                msg = 'unable to login with given creds'
+                raise exceptions.ValidationError(msg)
+        else:
+            msg = 'Username or Passwords are both required !!!'
+            raise exceptions.ValidationError(msg)
+        return data
+
+
+class ChangePasswordSerializer(serializers.Serializer):
+    old_password = serializers.CharField(required=True)
+    new_password = serializers.CharField(required=True)
+
